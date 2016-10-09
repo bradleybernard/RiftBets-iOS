a7eefca1a02dd400e504c19b4a9d8d8e577ff82c
Sushil Patel <sp1395@gmail.com>
2016-10-08 22:33:20 -0700

    Fb app start

--- RiftBets/AppDelegate.swift
+++ RiftBets/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         Fabric.with([Crashlytics.self])
-        return true
+        return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
     func applicationWillResignActive(application: UIApplication) {
@@ -42,6 +42,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        let loginManager: FBSDKLoginManager = FBSDKLoginManager()
+        loginManager.logOut()
     }
 
 
---
+++ RiftBets/FBIntegrationWithSwift-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  FBIntegrationWithSwift-Bridging-Header.h
+//  RiftBets
+//
+//  Created by Sushil Patel on 10/8/16.
+//  Copyright © 2016 Brad Bernard. All rights reserved.
+//
+
+#ifndef FBIntegrationWithSwift_Bridging_Header_h
+#define FBIntegrationWithSwift_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* FBIntegrationWithSwift_Bridging_Header_h */
+

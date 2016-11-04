857d29da7410a15aaabe3a9328134b50afee6fa4
Sushil Patel <sp1395@gmail.com>
2016-11-03 19:41:59 -0700

    Added to Main.storyboard under Match Detail Controller

---
+++ RiftBets/MatchDetailsCustomCell.swift
@@ -0,0 +1,16 @@
+//
+//  MatchDetailsCustomCell.swift
+//  RiftBets
+//
+//  Created by Sushil Patel on 11/3/16.
+//  Copyright © 2016 Brad Bernard. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MatchDetailsCustomCell: UITableViewCell{
+    
+    
+    
+}
